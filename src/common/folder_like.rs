5d1136a25eb37a63de96d9f2697e661366d2a7cd
Edoardo Morandi <morandidodo@gmail.com>
2025-07-16 00:04:51 +0200

    test: add for FolderLike::ancestor_count

--- src/common/folder_like.rs
+++ src/common/folder_like.rs
@@ -8,3 +8,30 @@ pub(super) trait FolderLike {
         iter::successors(self.parent(), |folder| folder.parent()).count()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::FolderLike;
+
+    #[derive(Debug, Default)]
+    struct Folder {
+        parent: Option<Box<Folder>>,
+    }
+
+    impl FolderLike for Folder {
+        fn parent(&self) -> Option<&Self> {
+            self.parent.as_deref()
+        }
+    }
+
+    #[test]
+    fn ancestor_count() {
+        let folder = Folder {
+            parent: Some(Box::new(Folder {
+                parent: Some(Box::new(Folder::default())),
+            })),
+        };
+
+        assert_eq!(folder.ancestor_count(), 2);
+    }
+}
