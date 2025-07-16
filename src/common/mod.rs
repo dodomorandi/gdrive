6c0e9510e7dea12d39e9136d4bd018b767d6b458
Edoardo Morandi <morandidodo@gmail.com>
2025-07-16 23:48:46 +0200

    feat: add parse_md5_digest fn

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -14,3 +14,28 @@ pub mod permission;
 pub mod table;
 
 use folder_like::FolderLike;
+
+#[expect(unused)]
+pub(crate) fn parse_md5_digest(s: &str) -> Option<md5::Digest> {
+    const MD5_LEN: usize = 16;
+
+    if s.len() != MD5_LEN * 2 {
+        return None;
+    }
+
+    let (chunks, _) = s.as_bytes().as_chunks::<2>();
+    let mut md5_bytes = [0; MD5_LEN];
+    chunks
+        .iter()
+        .map(|bytes| {
+            let s = std::str::from_utf8(bytes).ok()?;
+            u8::from_str_radix(s, 16).ok()
+        })
+        .zip(&mut md5_bytes)
+        .try_for_each(|(byte, out)| {
+            *out = byte?;
+            Some(())
+        })?;
+
+    Some(md5::Digest(md5_bytes))
+}
