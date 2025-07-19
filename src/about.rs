3235aadffa7cd59af0dffd57c78252157205e304
Edoardo Morandi <morandidodo@gmail.com>
2025-07-19 22:58:23 +0200

    style: split string into multiple lines in about

--- src/about.rs
+++ src/about.rs
@@ -4,5 +4,8 @@ pub fn about() {
     println!(
         "For the latest information check out the project page: https://github.com/glotlabs/gdrive"
     );
-    println!("You will also find link to the community chat and information on how to support the project.");
+    println!(
+        "You will also find link to the community chat and information on how to support the\
+        project."
+    );
 }
