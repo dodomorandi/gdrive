9ddcb88bbab5a316b7b181c37b97fdb9399529ed
Edoardo Morandi <morandidodo@gmail.com>
2025-08-13 23:49:00 +0200

    feat: simplify error handling in account::switch

--- src/account/switch.rs
+++ src/account/switch.rs
@@ -3,6 +3,7 @@ use crate::app_config::AppConfig;
 use std::error;
 use std::fmt::Display;
 use std::fmt::Formatter;
+use std::ops::Not;
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -11,7 +12,10 @@ pub struct Config {
 
 pub fn switch(config: &Config) -> Result<(), Error> {
     let accounts = app_config::list_accounts().map_err(Error::AppConfig)?;
-    err_if_account_not_found(&accounts, &config.account_name)?;
+
+    if accounts.contains(&config.account_name).not() {
+        return Err(Error::AccountNotFound(config.account_name.clone()));
+    }
 
     let app_cfg = AppConfig::init_account(&config.account_name).map_err(Error::AppConfig)?;
     app_config::switch_account(&app_cfg).map_err(Error::AppConfig)?;
@@ -34,13 +38,5 @@ impl Display for Error {
             Error::AppConfig(e) => write!(f, "{e}"),
             Error::AccountNotFound(name) => write!(f, "Account '{name}' not found"),
         }
-    }
-}
-
-fn err_if_account_not_found(accounts: &[String], account_name: &str) -> Result<(), Error> {
-    if accounts.contains(&account_name.to_string()) {
-        Ok(())
-    } else {
-        Err(Error::AccountNotFound(account_name.to_string()))
     }
 }
