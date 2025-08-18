1199f3a426f78eea5fa12df4659f446f413ea188
Edoardo Morandi <morandidodo@gmail.com>
2025-08-18 14:45:31 +0200

    feat: add FileIdentifier helper
    This simple structure can be used to display the name of a drive file
    when displaying an error.

--- src/common/file_tree_drive/errors.rs
+++ src/common/file_tree_drive/errors.rs
@@ -17,3 +17,59 @@ impl Error for FileTreeDrive {
         Some(&self.0)
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum FileIdentifier {
+    Name(String),
+    Id(String),
+    None,
+}
+
+impl FileIdentifier {
+    #[must_use]
+    pub fn new(name: Option<String>, id: Option<String>) -> Self {
+        if let Some(name) = name {
+            Self::Name(name)
+        } else if let Some(id) = id {
+            Self::Id(id)
+        } else {
+            Self::None
+        }
+    }
+
+    #[must_use]
+    pub fn display(&self) -> FileIdentifierDisplay<'_> {
+        FileIdentifierDisplay(self)
+    }
+}
+
+impl From<google_drive3::api::File> for FileIdentifier {
+    fn from(value: google_drive3::api::File) -> Self {
+        Self::new(value.name, value.id)
+    }
+}
+
+impl From<&google_drive3::api::File> for FileIdentifier {
+    fn from(value: &google_drive3::api::File) -> Self {
+        if let Some(name) = &value.name {
+            Self::Name(name.clone())
+        } else if let Some(id) = &value.id {
+            Self::Id(id.clone())
+        } else {
+            Self::None
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct FileIdentifierDisplay<'a>(&'a FileIdentifier);
+
+impl Display for FileIdentifierDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            FileIdentifier::Name(name) => write!(f, " with name '{name}'"),
+            FileIdentifier::Id(id) => write!(f, " with id '{id}'"),
+            FileIdentifier::None => Ok(()),
+        }
+    }
+}
